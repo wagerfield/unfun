4741173d4a6f9ad4562c8599ebd3c50e61f4d47d
Matthew Wagerfield <matthew@wagerfield.com>
2024-07-16 09:19:45 +0100

    feat: refine base types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,29 +1,20 @@
-import { __ } from "./__"
-
 export type Nil = null | undefined
 
-export type Placeholder = typeof __
+export type List<T = any> = string | T[]
 
 export type AnyObject = Record<string, any>
 
 export type AnyFunction = (...args: any[]) => any
 
-export type Predicate<T> = (value: T) => boolean
+export type Predicate<T> = (val: T) => boolean
 
-export type Keys<T extends object> = (keyof T)[]
+export type Keys<T extends {}> = (keyof T)[]
 
-export type Values<T extends object> = T[keyof T][]
+export type Values<T extends {}> = T[keyof T][]
 
-export type Entries<T extends object> = {
+export type Entries<T extends {}> = {
   [K in keyof T]: [K, T[K]]
 }[keyof T][]
-
-export type Iterator<T, U> = (
-  item: T,
-  index: number,
-  length: number,
-  array: ArrayLike<T>,
-) => U
 
 export type RequiredFirstParam<Func extends AnyFunction> =
   Parameters<Func> extends [infer Head, ...infer Tail]
