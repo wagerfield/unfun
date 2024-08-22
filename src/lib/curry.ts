d502982deedc37341d59b21336ec19b489f40870
Matthew Wagerfield <matthew@wagerfield.com>
2024-08-22 16:31:51 +0100

    feat: update curry function types

--- src/lib/curry.ts
+++ src/lib/curry.ts
@@ -1,10 +1,10 @@
-import type { AnyFunction, CurriedFunction, RequiredFirstParam } from "./types"
+import type { AnyFunction, CurryParameters } from "./types"
 
 export function curry<F extends AnyFunction>(
   fn: F,
   ...args: Partial<Parameters<F>>
-): CurriedFunction<F> {
-  return (...nextArgs: RequiredFirstParam<F>) => {
+) {
+  return <P extends CurryParameters<Parameters<F>>>(...nextArgs: P) => {
     const allArgs = [...args, ...nextArgs]
 
     return allArgs.length < fn.length
