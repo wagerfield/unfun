c7eb3bbe77005578ba1127378169a45d16907ae9
Matthew Wagerfield <matthew@wagerfield.com>
2024-08-22 16:31:04 +0100

    feat: define Head, Tail and CurryParameters types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,3 +1,5 @@
+import type { __ } from "./__"
+
 export type Nil = null | undefined
 
 export type List<T = any> = string | T[]
@@ -16,29 +18,21 @@ export type Entries<T extends {}> = {
   [K in keyof T]: [K, T[K]]
 }[keyof T][]
 
-export type RequiredFirstParam<Func extends AnyFunction> =
-  Parameters<Func> extends [infer Head, ...infer Tail]
-    ? [Head, ...Partial<Tail>]
-    : []
+export type Head<T extends readonly unknown[]> = T extends [
+  ...infer U extends [unknown],
+  ...unknown[],
+]
+  ? U
+  : never
 
-export type RemainingParams<
-  AppliedParams extends any[],
-  ExpectedParams extends any[],
-> = AppliedParams extends [any, ...infer AppliedTail]
-  ? ExpectedParams extends [any, ...infer ExpectedTail]
-    ? RemainingParams<AppliedTail, ExpectedTail>
-    : []
-  : ExpectedParams
+export type Tail<T extends readonly unknown[]> = T extends [unknown, ...infer U]
+  ? U
+  : never
 
-export type CurriedFunction<Func extends AnyFunction> = <
-  AppliedParams extends RequiredFirstParam<Func>,
-  ExpectedParams extends Parameters<Func>,
->(
-  ...args: AppliedParams
-) => RemainingParams<AppliedParams, ExpectedParams> extends [any, ...any[]]
-  ? CurriedFunction<
-      (
-        ...args: RemainingParams<AppliedParams, ExpectedParams>
-      ) => ReturnType<Func>
-    >
-  : ReturnType<Func>
+export type WithPlaceholder<T extends readonly unknown[]> = {
+  [K in keyof T]: T[K] | __
+}
+
+export type CurryParameters<T extends readonly unknown[]> = WithPlaceholder<
+  [...Head<T>, ...Partial<Tail<T>>]
+>
